6c6b80d5b7de33f694819defcbf7a059a77a1d70
Simon Koch <simon.koch@tu-braunschweig.de>
2024-06-29 17:55:08 +0200

    removed debug output

--- src/main/scala/de/halcony/appanalyzer/platform/appium/Appium.scala
+++ src/main/scala/de/halcony/appanalyzer/platform/appium/Appium.scala
@@ -204,11 +204,8 @@ object Appium extends LogSupport {
     }
     try {
       try {
-        debug("starting appium server")
         appium.startAppiumServer(conf.appium)
-        debug("appium server started, connecting ...")
         appium.connect(appId)
-        debug("connected to appium")
       } catch {
         case x: Throwable =>
           error(s"encountered appium start error:\n ${x.getMessage}")
