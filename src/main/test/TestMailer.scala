4b5d0ac03266d4b6e609ea5d2c2ababb75d42e3c
Jannes Schütt <jannes.schuett@tu-braunschweig.de>
2024-10-18 15:03:48 +0200

    Mailer test

---
+++ src/main/test/TestMailer.scala
@@ -0,0 +1,13 @@
+import de.halcony.appanalyzer.{Config, Mailer}
+import de.halcony.appanalyzer.analysis.plugin.RemotePluginConfig
+import org.scalatest._
+import org.scalatest.flatspec._
+import org.scalatest.matchers._
+
+class TestMailer extends AnyFlatSpec with should.Matchers {
+  "Mailer" should "send email" in {
+    val conf = Config.parse("config.json")
+    val mailer = new Mailer(conf.email.get)
+    mailer.send_email(subject = "Test", content = "Test")
+  }
+}
