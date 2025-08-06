ad2f085d6d7f36f99834d4d44ee31a8ecd7d3806
simon <simon.koch@uibk.ac.at>
2025-08-06 20:56:15 +0200

    scalafmt

--- src/main/scala/de/halcony/appanalyzer/appbinary/ipa/IPA.scala
+++ src/main/scala/de/halcony/appanalyzer/appbinary/ipa/IPA.scala
@@ -14,10 +14,12 @@ import scala.sys.process._
 case class IPA(conf: Config) extends Analysis with LogSupport {
 
   /** retrieve the list of files included in the IPA archive using zipinfo
-  *
-  * @param path the path to the IPA file
-  * @return a list of file paths contained in the IPA archive
-  */
+    *
+    * @param path
+    *   the path to the IPA file
+    * @return
+    *   a list of file paths contained in the IPA archive
+    */
   override def getIncludedFiles(path: String): List[String] = {
     val zipinfo =
       "zipinfo" // for now we are assuming this is installed - if this bites you later on ... well ...
@@ -31,11 +33,14 @@ case class IPA(conf: Config) extends Analysis with LogSupport {
   }
 
   /** execute a block of code with a resource that will be automatically closed
-  *
-  * @param resource the resource to be used and closed
-  * @param block the block of code to execute with the resource
-  * @return the result of the block execution
-  */
+    *
+    * @param resource
+    *   the resource to be used and closed
+    * @param block
+    *   the block of code to execute with the resource
+    * @return
+    *   the result of the block execution
+    */
   private def using[T <: { def close(): Unit }, U](
       resource: T
   )(block: T => U): U = {
@@ -49,18 +54,22 @@ case class IPA(conf: Config) extends Analysis with LogSupport {
   }
 
   /** perform cleanup operations
-  *
-  * No cleanup actions are performed in this case
-  */
+    *
+    * No cleanup actions are performed in this case
+    */
   override def cleanUp(): Unit = {}
 
   /** retrieve the application ID from the IPA's iTunesMetadata.plist
-  *
-  * @param app the MobileApp instance containing the IPA file path
-  * @param default an optional default value to return if metadata is missing
-  * @return the application ID extracted from the IPA metadata
-  * @throws FatalError if the metadata is missing and no default value is provided
-  */
+    *
+    * @param app
+    *   the MobileApp instance containing the IPA file path
+    * @param default
+    *   an optional default value to return if metadata is missing
+    * @return
+    *   the application ID extracted from the IPA metadata
+    * @throws FatalError
+    *   if the metadata is missing and no default value is provided
+    */
   override def getAppId(app: MobileApp, default: Option[String]): String = {
     using(new ZipFile(new File(app.path))) { zipFile =>
       val metadata = Option(zipFile.getEntry("iTunesMetadata.plist"))
@@ -84,21 +93,25 @@ case class IPA(conf: Config) extends Analysis with LogSupport {
   }
 
   /** retrieve the application ID from the IPA file using its path
-  *
-  * This method is not implemented
-  *
-  * @param path the path to the IPA file
-  * @return always throws NotImplementedError
-  */
+    *
+    * This method is not implemented
+    *
+    * @param path
+    *   the path to the IPA file
+    * @return
+    *   always throws NotImplementedError
+    */
   override def getAppId(path: String): String = throw new NotImplementedError()
 
   /** retrieve the application version from the IPA file
-  *
-  * This method is not implemented
-  *
-  * @param path the path to the IPA file
-  * @return always throws NotImplementedError
-  */
+    *
+    * This method is not implemented
+    *
+    * @param path
+    *   the path to the IPA file
+    * @return
+    *   always throws NotImplementedError
+    */
   override def getAppVersion(path: String): String =
     throw new NotImplementedError()
 }
--- src/main/scala/de/halcony/appanalyzer/appbinary/ipa/iTunesMetaDataParser.scala
+++ src/main/scala/de/halcony/appanalyzer/appbinary/ipa/iTunesMetaDataParser.scala
@@ -5,20 +5,24 @@ import scala.xml.Elem
 object iTunesMetaDataParser {
 
   /** parse the key from the given XML element
-  *
-  * @param xml the XML element expected to have the label "key"
-  * @return the text content of the key element
-  */
+    *
+    * @param xml
+    *   the XML element expected to have the label "key"
+    * @return
+    *   the text content of the key element
+    */
   def parseKey(xml: Elem): String = {
     assert(xml.label == "key")
     xml.text
   }
 
   /** parse a dictionary from the given XML element
-  *
-  * @param xml the XML element expected to have the label "dict"
-  * @return a MetaDict representing the parsed dictionary
-  */
+    *
+    * @param xml
+    *   the XML element expected to have the label "dict"
+    * @return
+    *   a MetaDict representing the parsed dictionary
+    */
   private def parseDict(xml: Elem): MetaDict = {
     assert(xml.label == "dict")
     val map = xml.child
@@ -33,30 +37,36 @@ object iTunesMetaDataParser {
   }
 
   /** parse an integer from the given XML element
-  *
-  * @param xml the XML element expected to have the label "integer"
-  * @return a MetaInteger representing the parsed integer value
-  */
+    *
+    * @param xml
+    *   the XML element expected to have the label "integer"
+    * @return
+    *   a MetaInteger representing the parsed integer value
+    */
   private def parseInteger(xml: Elem): MetaInteger = {
     assert(xml.label == "integer")
     MetaInteger(xml.text.toInt)
   }
 
   /** parse a string from the given XML element
-  *
-  * @param xml the XML element expected to have the label "string"
-  * @return a MetaString representing the parsed string value
-  */
+    *
+    * @param xml
+    *   the XML element expected to have the label "string"
+    * @return
+    *   a MetaString representing the parsed string value
+    */
   private def parseString(xml: Elem): MetaString = {
     assert(xml.label == "string")
     MetaString(xml.text)
   }
 
   /** parse a boolean value from the given XML element
-  *
-  * @param xml the XML element expected to have the label "true" or "false"
-  * @return a MetaBoolean representing the parsed boolean value
-  */
+    *
+    * @param xml
+    *   the XML element expected to have the label "true" or "false"
+    * @return
+    *   a MetaBoolean representing the parsed boolean value
+    */
   private def parseBoolean(xml: Elem): MetaBoolean = {
     assert(xml.label == "true" || xml.label == "false")
     val value = xml.label match {
@@ -67,19 +77,24 @@ object iTunesMetaDataParser {
   }
 
   /** parse an unknown XML element into a MetaUnknown
-  *
-  * @param elem the XML element with an unrecognized label
-  * @return a MetaUnknown containing the element's label and text content
-  */
+    *
+    * @param elem
+    *   the XML element with an unrecognized label
+    * @return
+    *   a MetaUnknown containing the element's label and text content
+    */
   private def parseUnknown(elem: Elem): MetaUnknown = {
     MetaUnknown(elem.label, elem.text)
   }
 
   /** parse the given XML element into an iTunesMetaDataElement
-  *
-  * @param xml the XML element to parse
-  * @return an iTunesMetaDataElement representing the parsed content of the XML element
-  */
+    *
+    * @param xml
+    *   the XML element to parse
+    * @return
+    *   an iTunesMetaDataElement representing the parsed content of the XML
+    *   element
+    */
   def parseElement(xml: Elem): iTunesMetaDataElement = {
     xml.label match {
       case "dict"           => parseDict(xml)
